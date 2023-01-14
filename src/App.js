2bb59e09b98f1cad6426d7a90a7b3bc4bd5f6289
David Monterroso Cabello <david.monterroso@tuispain.com>
2023-01-14 14:56:26 +0100

    initial config for tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col items-center mt-16 bg-black">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
